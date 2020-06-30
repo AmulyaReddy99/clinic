1c942139a1dc168777479cb1c047a5a94666bc6d
AmulyaReddy99 <amulyareddyk@yahoo.in>
2020-06-30 12:19:20 +0530

    created 3 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarModule } from 'ng-sidebar';
+import { DetailsComponent } from './details/details.component';
+import { StatsComponent } from './stats/stats.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    StatsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
