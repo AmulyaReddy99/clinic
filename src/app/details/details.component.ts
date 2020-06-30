7b77a7eafbc6fc5722ee79f6cca420898848db75
AmulyaReddy99 <amulyareddyk@yahoo.in>
2020-06-30 18:46:00 +0530

    added dynamic table

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as info from './info.json';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  status = ['open','closed'];
+  clinic_types = [];
+  distances = ["1 km","2 km","5 km"];
+  clinic_info = [];
+  
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
+    // clinic types and segregate data to rows
+    let c = 0;
+    for(let i=0; i<this.distances.length; i++){
+      let clinic = info["default"][this.distances[i]];
+      for(let j=0; j<clinic.length; j++){
+        let type = clinic[j]['type'];
+        this.clinic_info.push([]);
+        this.clinic_info[c].push(clinic[j]['clinic name']);
+        this.clinic_info[c].push(clinic[j]['type']);
+        this.clinic_info[c].push(clinic[j]['status']);
+        this.clinic_info[c].push(clinic[j]['ph no']);
+        this.clinic_info[c].push(clinic[j]['address']);
+        this.clinic_info[c].push(this.distances[i]);
+        c++;
+        if(!this.clinic_types.includes(type))
+          this.clinic_types.push(type);
+      }
+    }
+    console.log(this.clinic_info);
+  }
+
+  get_clinic_info(){
+    return this.clinic_info;
+  }
+
+  get_distances(){
+    return this.distances;
+  }
+
+  get_status(){
+    return this.status;
+  }
+
+  get_clinic_types(){
+    return this.clinic_types;
   }
 
 }
