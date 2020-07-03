30a498f717f8606cd5ffaa2792f8467c01be29d2
AmulyaReddy99 <amulyareddyk@yahoo.in>
2020-07-03 19:57:42 +0530

    messages added

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -12,4 +12,20 @@ export class MessagesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  current_message_list = '';
+  current_name = ''
+  
+  messages = {
+    'Rachana': ['Hello.. Hw r ya?','cool','hw is it in bangalore','fine','how about hitachi?'],
+    'Sreevani': ['I am fine..','cool','hw is jpmc','good'],
+    'Taruni': ['Happy birthday bro!'],
+    'Rajendra': ['Ok'],
+    'Pranav': ['Met charan yesterday','nice']
+  }
+
+  set_current_name(name){
+    this.current_name = name;
+    this.current_message_list = this.messages[this.current_name];
+  }
+
 }
